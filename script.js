650e3009bd62f7d25357d1d884cca10ba3255aa2
adamr-8 <adamlreed@gmail.com>
2024-08-17 21:48:54 -0400

    no idea
    shrugs

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ document.querySelectorAll('.circle').forEach(circle => {
         const discipline = this.getAttribute('data-discipline');
         contentElement.querySelector('h1').textContent = discipline;
         contentElement.querySelector('p').textContent = `${discipline} Content`;
+
+        // Move clicked circle to the center
+        document.querySelectorAll('.circle').forEach(c => c.style.transform = 'translate(-50%, -50%) scale(1)');
+        this.style.transform = 'translate(-50%, -50%) scale(1.25)';
     });
 });

cf77081d4d2cc0dea6fe9cfcc4fc97b0186d3212
adamr-8 <adamlreed@gmail.com>
2024-08-17 22:01:23 -0400

    ???
    crazy how this isn't working

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
 document.querySelectorAll('.circle').forEach(circle => {
-    circle.addEventListener('mouseover', function () {
-        const contentElement = document.querySelector('.content');
-        const discipline = this.getAttribute('data-discipline');
-        contentElement.querySelector('h1').textContent = discipline;
-        contentElement.querySelector('p').textContent = `${discipline} Content`;
-
-        // Move hovered circle to the center
-        const centerCircle = document.querySelector('.circle:nth-child(1)');
-        centerCircle.style.top = this.style.top;
-        centerCircle.style.left = this.style.left;
-        centerCircle.style.backgroundColor = this.style.backgroundColor;
-        centerCircle.innerHTML = this.innerHTML;
-        
-        this.style.top = '50%';
-        this.style.left = '50%';
-        this.style.transform = 'translate(-50%, -50%) scale(1.25)';
+    circle.addEventListener('mouseenter', function() {
+        document.getElementById('content-title').textContent = this.textContent;
+        document.getElementById('content-description').textContent = this.textContent + " Content";
     });
 });

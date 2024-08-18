4b07223c037919990a7b23bffacdd16b6ccbd2c9
adamr-8 <adamlreed@gmail.com>
2024-08-17 21:53:19 -0400

    VB-2.2
    crosses fingers again

--- script.js
+++ script.js
@@ -5,8 +5,15 @@ document.querySelectorAll('.circle').forEach(circle => {
         contentElement.querySelector('h1').textContent = discipline;
         contentElement.querySelector('p').textContent = `${discipline} Content`;
 
-        // Move clicked circle to the center
-        document.querySelectorAll('.circle').forEach(c => c.style.transform = 'translate(-50%, -50%) scale(1)');
+        // Move hovered circle to the center
+        const centerCircle = document.querySelector('.circle:nth-child(1)');
+        centerCircle.style.top = this.style.top;
+        centerCircle.style.left = this.style.left;
+        centerCircle.style.backgroundColor = this.style.backgroundColor;
+        centerCircle.innerHTML = this.innerHTML;
+        
+        this.style.top = '50%';
+        this.style.left = '50%';
         this.style.transform = 'translate(-50%, -50%) scale(1.25)';
     });
 });

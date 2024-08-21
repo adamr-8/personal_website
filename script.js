d98d0c87b057174f367cb3c55631b5ce4773ab71
adamr-8 <adamlreed@gmail.com>
2024-08-21 18:36:02 -0400

    8---9

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-document.querySelectorAll('.circle-button').forEach(button => {
-    button.addEventListener('click', function() {
-        const detail = this.getAttribute('data-detail');
-        window.location.href = `detail.html?detail=${detail}`;
+document.querySelectorAll('.circle-item').forEach(item => {
+    item.addEventListener('click', () => {
+        window.location.href = 'detail.html';
     });
 });

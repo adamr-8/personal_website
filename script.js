48b3021fa8882c4e13b71d65ff3b625b948c496a
adamr-8 <adamlreed@gmail.com>
2024-08-26 10:14:35 -0400

    9.301.01

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('.button');
 
     buttons.forEach(button => {
+        // Handle hover to open button
         button.addEventListener('mouseover', () => {
             buttons.forEach(btn => {
                 btn.style.zIndex = "0";
@@ -20,11 +21,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
         button.dataset.initialTransform = button.style.transform;
 
+        // Handle click to toggle open/close
         button.addEventListener('click', (e) => {
-            // Prevent the click from propagating to other elements
+            // Prevent the click from closing the button if it's already open
             e.stopPropagation();
 
-            // Toggle the open/close state of the button
             if (button.style.transform.includes('scale(8)') || button.style.transform.includes('scale(12)')) {
                 button.style.transform = button.dataset.initialTransform;
                 button.querySelector('.button-content').style.display = 'none';
